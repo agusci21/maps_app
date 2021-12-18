7fa67f77b8fcb2d199af7351dc35ce8e5afe4571
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-12-18 18:48:55 -0300

    Eventos creacion

--- lib/blocs/gps/gps_bloc.dart
+++ lib/blocs/gps/gps_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
 
 part 'gps_event.dart';
 part 'gps_state.dart';
@@ -11,5 +12,25 @@ class GpsBloc extends Bloc<GpsEvent, GpsState> {
     on<GpsAndPermissionEvent>((event, emit) => emit(state.copyWith(
         isGpsEnable: event.isGpsEnable,
         isGpsPermissionGaranted: event.isGpsPermissionsGaranted)));
+    _init();
+  }
+  Future<void> _init() async {
+    final isEnabled = await _checkGpsStatus();
+    print(isEnabled);
+  }
+
+  Future<bool> _checkGpsStatus() async {
+    final isEnable = await Geolocator.isLocationServiceEnabled();
+    Geolocator.getServiceStatusStream().listen((event) {
+      final isEnabled = (event.index == 1) ? true : false;
+      print(isEnabled);
+    });
+    return isEnable;
+  }
+
+  @override
+  Future<void> close() {
+    //TODO limpiar listener
+    return super.close();
   }
 }
