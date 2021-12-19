2eb0380ecedb0b83a3069cd0e0567675112c7b22
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-12-19 12:03:52 -0300

    Evento dentro del bloc

--- lib/blocs/gps/gps_bloc.dart
+++ lib/blocs/gps/gps_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:geolocator/geolocator.dart';
@@ -6,6 +8,7 @@ part 'gps_event.dart';
 part 'gps_state.dart';
 
 class GpsBloc extends Bloc<GpsEvent, GpsState> {
+  StreamSubscription? gpsServiceSuscription;
   GpsBloc()
       : super(const GpsState(
             isGpsEnable: false, isGpsPermissionGaranted: false)) {
@@ -17,20 +20,28 @@ class GpsBloc extends Bloc<GpsEvent, GpsState> {
   Future<void> _init() async {
     final isEnabled = await _checkGpsStatus();
     print(isEnabled);
+
+    add(GpsAndPermissionEvent(
+        isGpsEnable: isEnabled,
+        isGpsPermissionsGaranted: state.isGpsPermissionGaranted));
   }
 
   Future<bool> _checkGpsStatus() async {
     final isEnable = await Geolocator.isLocationServiceEnabled();
-    Geolocator.getServiceStatusStream().listen((event) {
+    gpsServiceSuscription = Geolocator.getServiceStatusStream().listen((event) {
       final isEnabled = (event.index == 1) ? true : false;
       print(isEnabled);
+      add(GpsAndPermissionEvent(
+          isGpsEnable: isEnabled,
+          isGpsPermissionsGaranted: state.isGpsPermissionGaranted));
     });
     return isEnable;
   }
 
   @override
   Future<void> close() {
-    //TODO limpiar listener
+    gpsServiceSuscription
+        ?.cancel(); // si gpsServiceSuscription tiene un valor (?) cancelalo, si no no hagas nada
     return super.close();
   }
 }
