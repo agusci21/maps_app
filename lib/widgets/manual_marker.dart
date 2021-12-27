f9941e2a3074b7f130d044db756e1b83b887634d
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-12-27 12:08:01 -0300

    Manual Marker

--- lib/widgets/manual_marker.dart
+++ lib/widgets/manual_marker.dart
@@ -12,6 +12,7 @@ class ManualMarker extends StatelessWidget {
       height: size.height,
       child: Stack(
         children: [
+          const Positioned(top: 70, left: 20, child: _ButtonBack()),
           Center(
             child: Transform.translate(
               offset: const Offset(0, -22),
@@ -24,7 +25,52 @@ class ManualMarker extends StatelessWidget {
               ),
             ),
           ),
+          Positioned(
+              bottom: 40,
+              left: 40,
+              child: FadeInUp(
+                duration: const Duration(milliseconds: 500),
+                child: MaterialButton(
+                  shape: const StadiumBorder(),
+                  height: 50,
+                  elevation: 0,
+                  color: Colors.black,
+                  onPressed: () {},
+                  minWidth: size.width - 120,
+                  child: const Text(
+                    'Confirmar Destino',
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300),
+                  ),
+                ),
+              ))
         ],
+      ),
+    );
+  }
+}
+
+class _ButtonBack extends StatelessWidget {
+  const _ButtonBack({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeInLeft(
+      duration: const Duration(milliseconds: 500),
+      child: CircleAvatar(
+        maxRadius: 25,
+        backgroundColor: Colors.white,
+        child: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_ios_new,
+            color: Colors.black,
+          ),
+          onPressed: () {},
+        ),
       ),
     );
   }
