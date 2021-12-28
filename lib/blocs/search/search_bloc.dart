79c5c44abefff6df3ec3fc8dae92c02db5e83b4c
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-12-28 10:04:56 -0300

    Conseguir las polylines

--- lib/blocs/search/search_bloc.dart
+++ lib/blocs/search/search_bloc.dart
@@ -1,6 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_polyline_algorithm/google_polyline_algorithm.dart';
+import 'package:mapas_app/models/models.dart';
 import 'package:mapas_app/services/services.dart';
 
 part 'search_event.dart';
@@ -17,8 +19,17 @@ class SearchBloc extends Bloc<SearchEvent, SearchState> {
         (event, emit) => emit(state.copyWith(isDisplayedManualMarked: false)));
   }
 
-  Future getCoorsStartToEnd(LatLng start, LatLng end) async {
-    final response = await trafficService.getCoorsStartToEnd(start, end);
-    return response;
+  Future<RouteDestination> getCoorsStartToEnd(LatLng start, LatLng end) async {
+    final trafficResponse = await trafficService.getCoorsStartToEnd(start, end);
+    final geometry = trafficResponse.routes.first.geometry;
+    final distance = trafficResponse.routes.first.distance;
+    final duration = trafficResponse.routes.first.duration;
+
+    final points = decodePolyline(geometry, accuracyExponent: 6);
+    final latLngList = points
+        .map((coors) => LatLng(coors[0].toDouble(), coors[1].toDouble()))
+        .toList();
+    return RouteDestination(
+        points: latLngList, duration: duration, distance: distance);
   }
 }
--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,3 +1,3 @@
 export 'package:mapas_app/models/search_result.dart';
 export 'package:mapas_app/models/traffic_response.dart';
-
+export 'package:mapas_app/models/route_destination.dart';
